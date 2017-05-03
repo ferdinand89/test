996e9ff7472a2bf78cba2d19d723217c13ff3d18
fred <fred@freds-mac.local>
2017-05-03 16:01:18 +0200

    Initial Commit

---
+++ RemeberThat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemeberThat
+//
+//  Created by fred on 03/05/17.
+//  Copyright © 2017 koenigsolution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
